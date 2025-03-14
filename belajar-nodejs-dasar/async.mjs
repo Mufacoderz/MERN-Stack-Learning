3f653ac380a96109a4acf50292808785337cc777
Muhammad <mf9555759@gmail.com>
2025-03-14 20:44:44 +0800

    os,path

--- belajar-nodejs-dasar/async.mjs
+++ belajar-nodejs-dasar/async.mjs
@@ -8,3 +8,6 @@ async function run() {
 }
 
 run()
+
+// Global async/await di dalam module
+// Di dalam file module (dengan ekstensi .mjs ), kita bisa langsung menggunakan "await" di level global tanpa perlu fungsi async.
---
+++ belajar-nodejs-dasar/path.mjs
@@ -0,0 +1,9 @@
+import path from "path";
+
+const file = "/Users/ucup/baba/asep.txt" ///contoh
+
+console.info(path.dirname(file));
+console.info(path.basename(file));
+console.info(path.extname(file));
+
+// digunakan utk mengekstrak informasi dari sebuah lokasi file

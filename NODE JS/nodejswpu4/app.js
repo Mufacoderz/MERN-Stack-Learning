3da79ace5c925dfc52103c8de5a063ff33104df8
Muhammad <mf9555759@gmail.com>
2025-03-27 20:15:54 +0800

    nodejswpu4

--- NODE JS/nodejswpu4/app.js
+++ NODE JS/nodejswpu4/app.js
@@ -4,10 +4,23 @@
 const fs = require('fs')
 
 
-//menuliskn string ke file secara syncronous
-fs.writeFileSync('tes.txt', 'hello world secara syncronous') 
-// formatnya ('nama file', 'isi data') == kalau nama file yg dicari tidak ada , maka akan otomatis dibuatkan
-// bisa juga ('namafolder/namafile', 'isi data') == kalau nama folder yg dicari tidak ada, maka akan eror, karena utk buat folder beda perintah
+// //menuliskn string ke file secara syncronous
+// fs.writeFileSync('tes.txt', 'hello world secara syncronous') 
+// // formatnya ('nama file', 'isi data') == kalau nama file yg dicari tidak ada , maka akan otomatis dibuatkan
+// // bisa juga ('namafolder/namafile', 'isi data') == kalau nama folder yg dicari tidak ada, maka akan eror, karena utk buat folder beda perintah
 
 
-// console.log(fs)
+// // console.log(fs)
+// try{
+    //     fs.writeFileSync('data/test.txt', ' Hello world secara sinkronus')
+    // } catch (e){
+        //     console.log(e)
+        // }
+
+
+
+ //menuliskn string ke file secara asyncronous
+
+ fs.writeFile('data/tes.txt', 'hellow world secara asycnronous', (e) => {
+    console.log(e)
+ })

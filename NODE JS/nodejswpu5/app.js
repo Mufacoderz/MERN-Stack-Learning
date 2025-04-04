4f2ad5ea3dc2fd4fbb200eb923a685a8bac104ab
Muhammad <mf9555759@gmail.com>
2025-04-04 19:40:39 +0800

    contoh pake package

--- NODE JS/nodejswpu5/app.js
+++ NODE JS/nodejswpu5/app.js
@@ -1,3 +1,11 @@
 //npm
+//contoh pack validator
 
-console.log('hello world')
+const validator = require('validator')
+const chalk = require('chalk')
+
+// console.log(validator.isEmail('fadil@gmail.com'))
+// console.log(validator.isMobilePhone('081234567', 'id-ID'))
+// console.log(validator.isNumeric('081234567sa'))
+
+console.log(chalk.black.bgBlue('hellow orld'))

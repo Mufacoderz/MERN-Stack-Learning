3317b4a7371e1ef5d97dffc368c555aa2656cfa8
Muhammad <mf9555759@gmail.com>
2025-04-19 20:33:05 +0800

    tambah fitur add menggunakan yargs npm

--- NODE JS/nodejswpu6/app.js
+++ NODE JS/nodejswpu6/app.js
@@ -1,13 +1,84 @@
 
-const {tulisPertanyaan, simpanContact} = require('./contacts')
 
-const main = async () => {
-    const nama = await tulisPertanyaan('Masukkan Nama anda : ')
-    const email = await tulisPertanyaan('Masukkan Email anda : ')
-    const noHP = await tulisPertanyaan('Masukkan nomor HP anda : ')
+//mengambil argumen dari command line
+
+// const command = process.argv[2]
+
+const yargs = require("yargs")
+
+
+// if(command == 'add'){
+
+// } else if (command == 'remove'){
+
+// } else if (command == 'list'){
+
+// }
+
+yargs.command({
+    command: 'add',
+    describe: 'menambahkan kontak baru',
+    builder: {
+        nama: {
+            describe: 'nama lengkap',
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: 'email',
+            demandOption: false,
+            type: 'string',
+        },
+        noHP: {
+            describe: 'nomor hp',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler(argv){
+        const contact = {
+            nama: argv.nama,
+            email: argv.email,
+            noHP: argv.noHP,
+        }
+        console.log(contact)
+    }
+})
+
+yargs.parse()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {tulisPertanyaan, simpanContact} = require('./contacts')
+
+// const main = async () => {
+//     const nama = await tulisPertanyaan('Masukkan Nama anda : ')
+//     const email = await tulisPertanyaan('Masukkan Email anda : ')
+//     const noHP = await tulisPertanyaan('Masukkan nomor HP anda : ')
     
-    simpanContact(nama, email, noHP)
-}
+//     simpanContact(nama, email, noHP)
+// }
 
-main()
+// main()
 

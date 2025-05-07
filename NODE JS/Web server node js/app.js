8f45fd1cce1d30ae803f08dc515cd3a0e43ed632
Muhammad <mf9555759@gmail.com>
2025-05-07 19:51:52 +0800

    route html pake func

--- NODE JS/Web server node js/app.js
+++ NODE JS/Web server node js/app.js
@@ -1,6 +1,17 @@
 const http = require('http')
 const port = 3000
 const fs = require('fs')
+const renderHTML =  (path, res) => {
+    fs.readFile(path, (err, data) => {
+        if(err){
+            res.writeHead(404)
+            res.write('file not found')
+        } else{
+            res.write(data)
+        }
+        res.end()
+    })
+}
 
 http
 .createServer((req, res) => {
@@ -10,22 +21,12 @@ http
 
     const url = req.url;
     if( url === '/about'){
-        res.write('<h1>Ini adalah page about</h1>');
-        res.end();
+        renderHTML('./about.html', res)
     } else if( url === '/contact'){
-        res.write('<h1>Ini adalah page contact</h1>');
-        res.end();
+        renderHTML('./contact.html', res)
     } else{
         // res.write('hello wolrd');
-        fs.readFile('./index.html', (err, data) => {
-            if(err){
-                res.writeHead(404)
-                res.write('file not found')
-            } else{
-                res.write(data)
-            }
-            res.end()
-        })
+        renderHTML('./index.html', res)
     }
 
 })

8e2017e6f710cce1247c60269248e0cc27ead37a
Muhammad <mf9555759@gmail.com>
2025-05-07 19:39:36 +0800

    web server

--- NODE JS/Web server node js/app.js
+++ NODE JS/Web server node js/app.js
@@ -1,8 +1,25 @@
 const http = require('http')
+const port = 3000
 
-http.createServer((req, res) => {
-    res.write('hello wolrd')
-    res.end()
-}).listen(3000, () => {
-    console.log("server is listening of port 3000")
+http
+.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html,'
+    });
+
+    const url = req.url;
+    if( url === '/about'){
+        res.write('<h1>Ini adalah page about</h1>');
+        res.end();
+    } else if( url === '/contact'){
+        res.write('<h1>Ini adalah page contact</h1>');
+        res.end();
+    } else{
+        res.write('hello wolrd');
+        res.end();
+    }
+
+})
+.listen(port, () => {
+    console.log(`server is listening on port ${port}`)
 })

13c29496ffd2ddd060ca1378c957e15e9b6917ab
Muhammad <mf9555759@gmail.com>
2025-06-01 19:21:58 +0800

    history

--- REACT JS/latihanTicTacToe/src/App.jsx
+++ REACT JS/latihanTicTacToe/src/App.jsx
@@ -10,11 +10,10 @@ function Square({value, onSquareClick}){
 }
 
 
-export default function Board() {
+function Board() {
   
   const [squares, setSquares] = useState(Array(9).fill(null))
 
-  const [xIsNext, setXIsNext] = useState(true)
 
   function handleClick(i){
 
@@ -55,6 +54,23 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
       </div>
     </>
+  )
+}
+
+export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true)
+
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  return(
+    <>
+      <div className='game'>
+        <div className="game-board">
+          <Board/>
+        </div>
+        <div className="game-info">
+          <ol></ol>
+        </div>
+      </div></>
   )
 }
 

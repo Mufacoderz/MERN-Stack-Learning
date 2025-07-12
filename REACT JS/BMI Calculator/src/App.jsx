9ce390f149bde7e5f452f5afea8a56d81cbe5896
Muhammad <mf9555759@gmail.com>
2025-07-12 20:06:32 +0800

    ui bmi calcu

--- REACT JS/BMI Calculator/src/App.jsx
+++ REACT JS/BMI Calculator/src/App.jsx
@@ -7,28 +7,36 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+
+      <div className='Container'>
+        <h2>BMI Calculator</h2>
+        <form>
+
+          <div>
+            <label>Weight (ibs)</label>
+            <input type="text" placeholder='Enter Weight value' value={weight} />
+          </div>
+
+          <div>
+            <label>Height (in)</label>
+            <input type="text" placeholder='Enter Height value' value={height} />
+          </div>
+
+          <div>
+            <button className='btn' type='submit'>Submit</button>
+            <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
+          </div>
+
+          <div className='center'>
+            <h3>Your BMI is:{bmi}</h3>
+            <p>{message}</p>
+          </div>
+
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 

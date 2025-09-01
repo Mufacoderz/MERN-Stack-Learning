73fc7fa2f03f8096ec1da9f96e970eb58d5235cc
Muhammad <mf9555759@gmail.com>
2025-09-01 19:33:24 +0800

    url

--- NODE JS/belajar-nodejs-dasar/net-server.mjs
+++ NODE JS/belajar-nodejs-dasar/net-server.mjs
@@ -8,3 +8,12 @@ const server = net.createServer((client)=>{
     })
 })
 server.listen(3000, "localhost")
+
+
+
+// Module 'net' adalah bawaan Node.js yang dipakai untuk membuat komunikasi jaringan berbasis TCP.
+// Dengan 'net' kita bisa bikin server dan client socket sederhana yang saling kirim data langsung.
+// Biasanya dipakai untuk:
+// 1. Bikin server custom berbasis TCP (contoh: chat server, game server).
+// 2. Komunikasi antar aplikasi tanpa HTTP.
+// 3. Menangani perangkat atau protokol custom yang kirim data lewat TCP.

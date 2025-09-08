4e6635ed056e62f0c24ae4c8e97ce27ceb097c05
Muhammad <mf9555759@gmail.com>
2025-09-08 16:03:10 +0800

    sync db

---
+++ CRUD MERN (MySql Express React Node)/backend/models/UserModel.js
@@ -0,0 +1,28 @@
+import { Sequelize } from "sequelize";
+import db from "../config/database.js";
+
+const { DataTypes } = Sequelize;
+
+const User = db.define(
+  "users",
+  {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    gender: DataTypes.STRING,
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+export default User;
+
+
+(async () => {
+  try {
+    await db.sync();
+    console.log("✅ Database synced successfully");
+  } catch (error) {
+    console.error("❌ Failed to sync database:", error.message);
+  }
+})();

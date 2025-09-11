d93df2646fa93f43ce2d1914ce4e8ddbbe583dbb
Muhammad <mf9555759@gmail.com>
2025-09-11 16:20:09 +0800

    done delete

--- CRUD MERN (MySql Express React Node)/frontend/src/components/UserList.jsx
+++ CRUD MERN (MySql Express React Node)/frontend/src/components/UserList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const UserList = () => {
   const [users, setUser] = useState([])
@@ -18,9 +19,19 @@ const UserList = () => {
     }
   }
 
+  const deleteUser = async (id)=>{
+    try {
+      await axios.delete(`http://localhost:500/users/${id}`)
+      getUser()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="columns mt-5 is-centered">
         <div className="column is-half ">
+          <Link to={`add`} className='button is-success'>Add New</Link>
             <table className='table is-striped is-fullwidth'>
                 <thead>
                     <tr>
@@ -39,8 +50,8 @@ const UserList = () => {
                             <td>{user.email}</td>
                             <td>{user.gender}</td>
                             <td>
-                                <button className='button is-small is-info'>Edit</button>
-                                <button className='button is-small is-danger'>Delete</button>
+                                <Link to={`edit/${user.id}`} className='button is-small is-info'>Edit</Link>
+                                <button onClick={()=> deleteUser(user.id)} className='button is-small is-danger'>Delete</button>
                             </td>
                      </tr>
                     ))}

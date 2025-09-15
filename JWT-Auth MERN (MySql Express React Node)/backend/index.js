a70965192e50af39a7b381bdef3046f4932fd6a9
Muhammad <mf9555759@gmail.com>
2025-09-15 15:17:04 +0800

    set db

--- JWT-Auth MERN (MySql Express React Node)/backend/index.js
+++ JWT-Auth MERN (MySql Express React Node)/backend/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import db from './config/database.js'
+import Users from './models/UserModel.js'
 const app = express()
+
+try {
+    await db.authenticate()
+    console.log('database connekted')
+    await Users.sync()
+} catch (error) {
+    console.log(error)
+}
 
 app.listen(5000, ()=> console.log('server runnign at port 5000'))

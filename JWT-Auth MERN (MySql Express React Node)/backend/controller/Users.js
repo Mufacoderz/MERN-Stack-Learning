b219d72509a00f6f70b17cd3fa33ea5e10d7f1e9
Muhammad <mf9555759@gmail.com>
2025-09-18 19:59:49 +0800

    login

--- JWT-Auth MERN (MySql Express React Node)/backend/controller/Users.js
+++ JWT-Auth MERN (MySql Express React Node)/backend/controller/Users.js
@@ -1,5 +1,6 @@
 import Users from '../models/UserModel.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export const getUsers = async(req, res)=>{
     try {
@@ -24,5 +25,38 @@ export const Register = async(req, res)=>{
         res.json({msg: 'register berhasil'})
     } catch (error) {
         console.log(error)
+    }
+}
+
+export const Login = async(res,req)=>{
+    try {
+        const user = await Users.findAll({
+            where:{
+                email: req.body.email
+            }
+        })
+        const match = await bcrypt.compare(req.body.password, user[0].password)
+        if(!match) return res.status(404).json({msg:'pw salah'})
+            const userId = user[0].id
+            const name = user[0].name
+            const email = user[0].email
+            const accessToken = jwt.sign({userId, name, email}, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '20s'
+            })
+            const refreshToken = jwt.sign({userId, name, email}, process.env.REFRESH_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            await Users.update({refresh_token: refreshToken},{
+                where:{
+                    id:userId
+                }
+            })
+            res.cookie('refreshToken', refreshToken,{
+                httpOnly: true,
+                maxAge: 24 * 60 * 60 * 1000,
+            })
+            res.json({accessToken})
+    } catch (error) {
+        res.status(404).json({msg:'email tak ditemukan'})
     }
 }
